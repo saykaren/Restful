96c130e2d3810e3d862bc20a91ee843374ba1651
Karen Garcia <saykaren@gmail.com>
2021-07-20 16:51:13 -0600

    Sets Proper Folders

---
+++ src/Components/App/App.js
@@ -0,0 +1,14 @@
+
+import '../Styling/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       Testing
+      </header>
+    </div>
+  );
+}
+
+export default App;
